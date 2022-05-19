17755e7fa94482be30c41cc4b0d606fffbb70db9
Dinostraw <105689004+Dinostraw@users.noreply.github.com>
2022-05-19 04:04:43 -0400

    Added some more plots to try

--- Samples/more_stats.py
+++ Samples/more_stats.py
@@ -2,6 +2,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from matplotlib.ticker import FuncFormatter
 
+from mariokart8.mk8 import MK8Tracks as Tracks
+from mariokart8.timesheet import format_time
+
+# Modify in between here =================================================================================
+DIRECTORY = "./Output/Boards/"
+FILENAME = "55 WW 2022-05-18 07-43.csv"
+WR = 104298  # WR time (in milliseconds)
+SG = 127868  # Staff Ghost time (in milliseconds)
+# ========================================================================================================
+LABELED_TIMES = {WR: "WR", SG: "SG"}
+
 
 def df_to_csv(df, *args):
     df.to_csv(''.join(x for x in args) + ".csv",
@@ -21,13 +32,6 @@ def filter_boards(df: pd.DataFrame, column, values):
     return df[df[column].isin(values)]
 
 
-def format_time(score):
-    millisec = score % 1000
-    seconds = score // 1000 % 60
-    minutes = score // 1000 // 60
-    return "%i:%02i.%03i" % (minutes, seconds, millisec)
-
-
 def prevent_excel_reformat(df, columns):
     for col in columns:
         if col in df.columns:
@@ -35,20 +39,27 @@ def prevent_excel_reformat(df, columns):
     return df
 
 
+def tick_formatter(x, _):
+    return LABELED_TIMES[x] if x in LABELED_TIMES else format_time(x)
+
+
 def main():
-    directory = "./Output/Boards/"
-    filename = "62 WW 2022-05-17 00-59.csv"
-
-    track_id, _, timestamp = filename.split(" ", maxsplit=2)
+    track_id, _, timestamp = FILENAME.split(" ", maxsplit=2)
     timestamp = timestamp[:-4]
+    track_name = "Unknown Track"
+    track_abbr = "???"
+    for track in Tracks:
+        if int(track_id) == track.id_:
+            track_name = track.fullname
+            track_abbr = track.abbr
 
-    df = pd.read_csv(directory + filename)
-    print(df.shape)
+    df = pd.read_csv(DIRECTORY + FILENAME)
+    # print(df.shape)
 
-    df_filtered = df[df["raw_time"] >= 102676]
+    df_filtered = df[df["raw_time"] >= WR]
     df_filtered["rank"] = df_filtered["rank"] - df.shape[0] + df_filtered.shape[0] + 1
-    print(df_filtered["country"].unique(), "\n")
-    print(df_filtered["country"].value_counts().to_string(), "\n")
+    # print(df_filtered["country"].unique(), "\n")
+    # print(df_filtered["country"].value_counts().to_string(), "\n")
 
     cols = ["rank", "time", "motion", "country", "nnid", "name"]
     prev_ref_cols = ["time"]  # Columns to prevent Excel from reformatting
@@ -81,61 +92,94 @@ def main():
 
     # Motion Controls Rankings
     df_mo = df_filtered[df_filtered["motion"]][cols].drop(columns="motion")
-    print(df_mo.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_mo, directory, track_id, " MO ", timestamp)
-    df_to_xlsx(df_mo, directory, track_id, " MO ", timestamp, prevent_reformat=True,
+    # print(df_mo.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_mo, DIRECTORY, track_id, " MO ", timestamp)
+    df_to_xlsx(df_mo, DIRECTORY, track_id, " MO ", timestamp, prevent_reformat=True,
                columns=prev_ref_cols)
 
     # American Regionals
     df_am = filter_boards(df_filtered, "country", am_set)[cols]
-    print(df_am.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_am, directory, track_id, " AM ", timestamp)
+    # print(df_am.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_am, DIRECTORY, track_id, " AM ", timestamp)
 
     # European Regionals
     df_eu = filter_boards(df_filtered, "country", eu_set)[cols]
-    print(df_eu.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_eu, directory, track_id, " EU ", timestamp)
+    # print(df_eu.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_eu, DIRECTORY, track_id, " EU ", timestamp)
 
     # Asian Regionals
     df_as = filter_boards(df_filtered, "country", as_set)[cols]
-    print(df_as.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_as, directory, track_id, " AS ", timestamp)
+    # print(df_as.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_as, DIRECTORY, track_id, " AS ", timestamp)
 
     # Middle Eastern Regionals
     df_me = filter_boards(df_filtered, "country", me_set)[cols]
-    print(df_me.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_me, directory, track_id, " ME ", timestamp)
+    # print(df_me.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_me, DIRECTORY, track_id, " ME ", timestamp)
 
     # African Regionals
     df_af = filter_boards(df_filtered, "country", af_set)[cols]
-    print(df_af.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_af, directory, track_id, " AF ", timestamp)
+    # print(df_af.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_af, DIRECTORY, track_id, " AF ", timestamp)
 
     # Oceanian Regionals
     df_oc = filter_boards(df_filtered, "country", oc_set)[cols]
-    print(df_oc.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_oc, directory, track_id, " OC ", timestamp)
+    # print(df_oc.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_oc, DIRECTORY, track_id, " OC ", timestamp)
 
     # Japanese Country Rankings
     df_jp = df_filtered[df_filtered["country"] == "JPN"][cols]
-    print(df_jp.to_string(index=False, max_rows=50), "\n")
-    df_to_csv(df_jp, directory, track_id, " JP ", timestamp)
+    # print(df_jp.to_string(index=False, max_rows=50), "\n")
+    df_to_csv(df_jp, DIRECTORY, track_id, " JP ", timestamp)
 
-    print(df_filtered[cols].tail(10).to_string(index=False))
+    # ====================
 
-    # avg_time_per_nat = df.groupby(["country"])["raw_time"].mean().sort_values()
-    # avgs_formatted = avg_time_per_nat.apply(format_time)
-    # print(avgs_formatted.to_string(header=False))
-    #
-    # ax = avg_time_per_nat.plot.bar(color=["deepskyblue", "royalblue"])
-    # ax.set_yticks([0, 30000, 60000, 90000, 102676, 120000, 150000,
-    #                180000, 210000, 240000, 270000, 30000])
-    # ax.yaxis.set_major_formatter(FuncFormatter(lambda x, _: format_time(x)))
-    # ax.axhline(y=102676, color="crimson", linewidth=3, label="WR")
-    # plt.margins(0, 0)
-    # plt.title("dCL Average Time per Country")
-    # plt.grid(axis='y', linestyle='--', color="black")
-    # plt.show()
+    tick_values = [WR, 0, 30000, 60000, 90000, 120000, 150000,
+                   180000, 210000, 240000, 270000, 300000]
+
+    # Average Time per Country Plot
+    avg_time_per_nat = df.groupby(["country"])["raw_time"].mean().sort_values()
+    avgs_formatted = avg_time_per_nat.apply(format_time)
+    print(avgs_formatted.to_string(header=False))
+
+    ax = avg_time_per_nat.plot.bar(color=["deepskyblue", "royalblue"])
+    ax.set_yticks(tick_values)
+    ax.yaxis.set_major_formatter(FuncFormatter(lambda x, _: format_time(x)))
+    ax.axhline(y=WR, color="crimson", linewidth=3, label="WR")
+    plt.margins(0, 0)
+    plt.title(f"{track_name} Average Time per Country")
+    plt.grid(axis='y', linestyle='--', color="black")
+    plt.show()
+
+    # Frequency That Each Unique Time Occurs
+    time_counts = df_filtered["raw_time"].value_counts().reindex(index=range(0, 300000), fill_value=0)
+    ax = time_counts.plot.line()
+    ax.set_xticks(tick_values)
+    ax.xaxis.set_major_formatter(FuncFormatter(tick_formatter))
+    plt.margins(0, 0)
+    plt.title("dIIO Frequency of Each Unique Time")
+    plt.show()
+
+    # Probability Distribution Approximation
+    ax = df_filtered["raw_time"].plot.kde()
+    ax.set_xticks(tick_values)
+    ax.xaxis.set_major_formatter(FuncFormatter(tick_formatter))
+    plt.margins(0, 0)
+    plt.title(f"{track_name} Time Density")
+    plt.show()
+
+    # Distribution per Interval of Time
+    ax = df_filtered["raw_time"].plot.hist(bins=300, figsize=(8, 4.5))
+    ax.set_xlim(left=0, right=299999)
+    ax.set_xticks(tick_values)
+    ax.xaxis.set_major_formatter(FuncFormatter(tick_formatter))
+    ax.tick_params(labelsize=6)
+    ax.axvline(x=SG, color="crimson", linewidth=1, linestyle="--", label="SG")
+    plt.margins(0, 0)
+    plt.title(f"{track_name} Time Distribution")
+    plt.savefig(f"./Output/Plots/{track_abbr} Time Distribution (1 sec).png",
+                dpi=1200, bbox_inches="tight")
+    plt.show()
 
 
 if __name__ == "__main__":

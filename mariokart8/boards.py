18feb51305077a1ca4341588a8efdfb333c1f0c9
Dinostraw <105689004+Dinostraw@users.noreply.github.com>
2022-06-06 00:38:35 -0400

    Added the ability to filter leaderboards

--- mariokart8/boards.py
+++ mariokart8/boards.py
@@ -1,16 +1,76 @@
 import asyncio
-from collections import ChainMap
+import json
+import os
+from collections import ChainMap, namedtuple
 from typing import Union
 
 from nintendo.nex import backend
-from nintendo.nex.ranking import RankingClient, RankingMode, RankingOrderParam, RankingResult
+from nintendo.nex.ranking import RankingClient, RankingMode, RankingOrderCalc
+from nintendo.nex.ranking import RankingOrderParam, RankingResult
 from nintendo.nnas import NNASClient
 
 from mariokart8.mk8 import MK8Client
 
+_AltInfo = namedtuple("_AltInfo", ["main", "alts"])
+with open(os.path.join(os.path.dirname(__file__), "./filters/alts.json"), 'r') as f:
+    _alts = {int(main): _AltInfo(True, [int(alt) for alt in alts])
+             for main, alts in json.load(f).items()}
+_alts.update({alt: _AltInfo(False, [main]) for main, info in _alts.items() for alt in info.alts})
 
-async def get_boards_stub(ranking_client: RankingClient, order_param: RankingOrderParam,
-                          track_id: int, pid: Union[int, None]) -> RankingResult:
+with open(os.path.join(os.path.dirname(__file__), "./filters/banned.txt"), 'r') as f:
+    _banned = {int(line.strip()) for line in f.readlines()}
+
+with open(os.path.join(os.path.dirname(__file__), "./filters/blacklist.json"), 'r') as f:
+    _blacklist = {int(main): {int(alt) for alt in alts} for main, alts in json.load(f).items()}
+
+with open(os.path.join(os.path.dirname(__file__), "./filters/hacked.txt"), 'r') as f:
+    _hacked = {int(line.strip()) for line in f.readlines()}
+
+
+def filter_boards(boards: RankingResult, hacked: bool = True, banned: bool = True,
+                  alts: bool = True, reindex: bool = True) -> RankingResult:
+    data = boards.data
+    tid = boards.data[0].category
+    if hacked:
+        data = list(filter(lambda x: x.pid not in _hacked, data))
+        data = list(filter(lambda x: tid not in _blacklist[x.pid] if x.pid in _blacklist else True, data))
+
+    if banned:
+        data = list(filter(lambda x: x.pid not in _banned, data))
+
+    if alts:
+        alts_set = set()
+        for x in data:
+            if x.pid not in _alts or x.pid in alts_set:
+                continue
+            if _alts[x.pid].main is True:
+                alts_set.update(_alts[x.pid].alts)
+            else:
+                main_pid = _alts[x.pid].alts[0]
+                alts_set.add(main_pid)
+                alts_set.update([pid for pid in _alts[main_pid].alts if x.pid != pid])
+        data = list(filter(lambda x: x.pid not in alts_set, data))
+
+    if reindex and (hacked or banned or alts) and len(data) > 0:
+        data[0].rank = 1
+        if len(data) > 1:
+            prev = data[0]
+            for i, x in enumerate(data[1:], start=2):
+                if x.score != prev.score:
+                    x.rank = i
+                else:
+                    x.rank = prev.rank
+                prev = x
+
+    result = RankingResult()
+    result.data = data
+    result.since_time = boards.since_time
+    result.total = boards.total
+    return result
+
+
+async def get_boards_stub(ranking_client: RankingClient, track_id: int, order_param: RankingOrderParam,
+                          pid: Union[int, None]) -> RankingResult:
     if order_param.count > 255:
         order_param.count = 255
     if pid:
@@ -26,11 +86,17 @@ async def get_boards_stub(ranking_client: RankingClient, order_param: RankingOrd
     return boards
 
 
-async def get_boards(client: MK8Client, order_param: RankingOrderParam, track_id: int,
+async def get_boards(client: MK8Client, track_id: int, offset: int, count: int,
                      pid: Union[int, None] = None) -> RankingResult:
     async with backend.connect(client.settings, client.nex_token.host, client.nex_token.port) as be:
         async with be.login(str(client.nex_token.pid), client.nex_token.password) as be_client:
             ranking_client = RankingClient(be_client)
+
+            order_param = RankingOrderParam()
+            order_param.order_calc = RankingOrderCalc.STANDARD
+            order_param.offset = offset
+            order_param.count = count
+
             # Necessary in the case that order_param.count exceeds 255
             remaining = order_param.count
 
@@ -38,7 +104,7 @@ async def get_boards(client: MK8Client, order_param: RankingOrderParam, track_id
             if pid:
                 order_param.offset = 1
 
-            boards = await get_boards_stub(ranking_client, order_param, track_id, pid)
+            boards = await get_boards_stub(ranking_client, track_id, order_param, pid)
 
             # Return early if requested amount is 255 or less
             if remaining <= 255:
@@ -56,7 +122,7 @@ async def get_boards(client: MK8Client, order_param: RankingOrderParam, track_id
                 order_param.count = remaining if remaining <= 255 else 255
                 pid = boards.data[-1].pid  # Get last PID in list to use as the base for the next request
 
-                frag = await get_boards_stub(ranking_client, order_param, track_id, pid)
+                frag = await get_boards_stub(ranking_client, track_id, order_param, pid)
                 boards.data.extend(frag.data[1:])
                 remaining -= 254
 

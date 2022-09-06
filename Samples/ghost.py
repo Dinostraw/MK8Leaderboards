59d985409eac2c654b18cd8b9820d1900ee9e647
Dinostraw <105689004+Dinostraw@users.noreply.github.com>
2022-09-06 03:27:53 -0400

    Downloaded ghosts are now properly saved with their header data; motion control usage is now properly reflected in the filenames of downloaded ghosts

--- Samples/ghost.py
+++ Samples/ghost.py
@@ -5,7 +5,8 @@ import anyio
 from anynet import http
 from dotenv import load_dotenv
 from nintendo.nex import backend, datastore
-from nintendo.nex.ranking import RankingClient, RankingMode, RankingOrderCalc, RankingOrderParam
+from nintendo.nex.ranking import (RankingClient, RankingMode, RankingOrderCalc,
+                                  RankingOrderParam, RankingRankData)
 
 from mk8boards.common import MK8Tracks as Tracks
 from mk8boards.mk8.boards_client import MK8Client
@@ -35,7 +36,21 @@ TRACK_ID = Tracks.GCN_YOSHI_CIRCUIT.id_
 NNID = "Dinostraw"  # NNID of targeted player
 
 
-async def get_common_data(client: MK8Client, nnid: str) -> bytes:
+class GhostResult:
+    def __init__(self, rankdata: RankingRankData, ghost_response: http.HTTPResponse):
+        self.pid = rankdata.pid
+        self.unique_id = rankdata.unique_id
+        self.rank = rankdata.rank
+        self.category = rankdata.category
+        self.score = rankdata.score
+        self.motion = bool(rankdata.groups[0])
+        self.param = rankdata.param
+        self.common_data = rankdata.common_data
+        self.ghost_data = ghost_response.body
+        self.upload_time = ghost_response.headers["Last-Modified"]
+
+
+async def get_ghost(client: MK8Client, nnid: str) -> GhostResult:
     order_param = RankingOrderParam()
     order_param.order_calc = RankingOrderCalc.STANDARD
     order_param.offset = 0
@@ -43,19 +58,16 @@ async def get_common_data(client: MK8Client, nnid: str) -> bytes:
 
     async with backend.connect(client.settings, client.nex_token.host, client.nex_token.port) as be:
         async with be.login(str(client.nex_token.pid), client.nex_token.password) as be_client:
+            pid = await client.nnas.get_pid(nnid)
+
             ranking_client = RankingClient(be_client)
-            rankings = await ranking_client.get_ranking(RankingMode.SELF, TRACK_ID, order_param,
-                                                        0, await client.nnas.get_pid(nnid))
-            return rankings.data[0].common_data
+            rankings = await ranking_client.get_ranking(RankingMode.SELF, TRACK_ID, order_param, 0, pid)
+            rankdata = rankings.data[0]
 
-
-async def get_ghost(client: MK8Client, nnid: str) -> bytes:
-    async with backend.connect(client.settings, client.nex_token.host, client.nex_token.port) as be:
-        async with be.login(str(client.nex_token.pid), client.nex_token.password) as be_client:
             store = datastore.DataStoreClient(be_client)
 
             get_param = datastore.DataStorePrepareGetParam()
-            get_param.persistence_target.owner_id = await client.nnas.get_pid(nnid)
+            get_param.persistence_target.owner_id = pid
             get_param.persistence_target.persistence_id = TRACK_ID - 16
             get_param.extra_data = ["WUP", str(REGION_ID), REGION_NAME, str(COUNTRY_ID), COUNTRY_NAME, ""]
 
@@ -64,7 +76,7 @@ async def get_ghost(client: MK8Client, nnid: str) -> bytes:
             response = await http.get(req_info.url, headers=headers)
             response.raise_if_error()
 
-            return response.body
+            return GhostResult(rankdata, response)
 
 
 async def main():
@@ -73,10 +85,14 @@ async def main():
                            USERNAME, PASSWORD)
     await mk8_client.login()
 
-    ghost_data = await get_ghost(mk8_client, NNID)
-    ghost_info = MK8GhostInfo(ghost_data)
+    ghost_result = await get_ghost(mk8_client, NNID)
+    ghost_info = MK8GhostInfo(ghost_result.ghost_data, motion=ghost_result.motion)
+    print("Ghost Data Information")
+    print("======================")
     print(ghost_info, end="\n\n")
+    print("Upload Timestamp:", ghost_result.upload_time, end="\n\n")
     print("Internal Mii Name:", ghost_info.mii.mii_name)
+    print(f"Internal Mii Birthday: {ghost_info.mii.birth_day}-{ghost_info.mii.birth_month}")
     print("Internal Mii Creator:", ghost_info.mii.creator_name)
     print("Internal Mii Creator ID:", ''.join(f"{x:02X}" for x in ghost_info.mii.author_id))
     print("Internal Mii MAC Address:", ''.join(f"{x:02X}" for x in ghost_info.mii.mac_address))
@@ -84,21 +100,24 @@ async def main():
           f"{''.join(f'{x:02x}' for x in ghost_info.mii.to_studio_api())}"
           f"&width=512&type=face", end="\n\n")
     filename = ghost_info.generate_filename()
-    print(f"Filename:\n{filename}")
+    print(f"Filename:\n{filename}\n\n")
     with open("../Output/Ghosts/" + filename, "wb") as f:
-        f.write(ghost_data)
+        f.write(ghost_info.header)
+        f.write(ghost_info.data)
 
-    common_data = await get_common_data(mk8_client, NNID)
-    player_info = MK8PlayerInfo(common_data)
+    player_info = MK8PlayerInfo(ghost_result.common_data)
+    print("Common Data Information")
+    print("=======================")
     print("Current Mii Name:", player_info.mii.mii_name)
-    print("Internal Mii Creator:", player_info.mii.creator_name)
-    print("Internal Mii Creator ID:", ''.join(f"{x:02X}" for x in player_info.mii.author_id))
-    print("Internal Mii MAC Address:", ''.join(f"{x:02X}" for x in player_info.mii.mac_address))
+    print(f"Current Mii Birthday: {player_info.mii.birth_day}-{player_info.mii.birth_month}")
+    print("Current Mii Creator:", player_info.mii.creator_name)
+    print("Current Mii Creator ID:", ''.join(f"{x:02X}" for x in player_info.mii.author_id))
+    print("Current Mii MAC Address:", ''.join(f"{x:02X}" for x in player_info.mii.mac_address))
     print(f"https://studio.mii.nintendo.com/miis/image.png?data="
           f"{''.join(f'{x:02x}' for x in player_info.mii.to_studio_api())}"
           f"&width=512&type=face")
     # with open("../Output/Common Data/common_data.bin", "wb") as f:
-    #     f.write(common_data)
+    #     f.write(ghost_result.common_data)
 
 
 anyio.run(main)

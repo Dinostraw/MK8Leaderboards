b7576a1aba8e3afc9f4f2df0add6f9afbf2fd6c7
Dinostraw <105689004+Dinostraw@users.noreply.github.com>
2023-01-16 23:46:28 -0500

    Initial migration to discord.py 2.0; updated to reflect new changes

--- discord_bot.py
+++ discord_bot.py
@@ -1,16 +1,18 @@
+import asyncio
 import logging
 import os
 
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
+from nintendo.nex.ranking import RankingClient, RankingRankData
 
 from mk8boards.common import MK8Cups, MK8Tracks
 from mk8boards.mk8 import timesheet as ts
 from mk8boards.mk8.boards_client import MK8Client
 from mk8boards.mk8.track_stats import get_stats, format_all_stats, stats_to_labeled_dict
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.WARNING)
 
 # Load environment variables
 load_dotenv()
@@ -29,21 +31,11 @@ LANGUAGE = os.getenv("LANGUAGE")
 USERNAME = os.getenv("NNID_USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 
-mk8_client = MK8Client(DEVICE_ID, SERIAL_NUMBER, SYSTEM_VERSION, COUNTRY_ID, COUNTRY_NAME,
-                       REGION_ID, REGION_NAME, LANGUAGE, USERNAME, PASSWORD)
-
-bot = commands.Bot(command_prefix='!')
 md_escape = {ord('*'): '\\*', ord('_'): '\\_', ord('~'): '\\~',
              ord('`'): '\\`', ord('>'): '\\>', ord('|'): '\\|'}
 
 
-@bot.event
-async def on_ready():
-    await mk8_client.login()
-    print("We up, boys!")
-
-
-def mark_time_if_faster(record1, record2):
+def mark_faster_time(record1: RankingRankData, record2: RankingRankData):
     if record1 is None:
         return "-:--.---"
     elif record2 is None or record1.score >= record2.score:
@@ -52,103 +44,138 @@ def mark_time_if_faster(record1, record2):
         return "<%s>" % ts.format_time(record1.score)
 
 
-@bot.command(aliases=["mu"], description="Gets the matchup between two players given their NNIDs")
-async def matchup(ctx, nnid1, nnid2):
-    embed = discord.Embed()
-    embed.title = f"Generating matchup between {nnid1} and {nnid2}..."
-    embed.description = "Please wait..."
-    message = await ctx.reply(embed=embed)
-    try:
-        m = ts.get_matchup(*(await ts.get_timesheets(mk8_client, [nnid1, nnid2])))
-        p1 = m.p1_timesheet
-        p2 = m.p2_timesheet
-        embed.title = (f"{p1.mii_name} ({p1.nnid})   vs.   {p2.mii_name} ({p2.nnid})"
-                       .translate(md_escape))
+class BotBoi(commands.Bot):
+    def __init__(self):
+        prefix = '!'
+        intents = discord.Intents.default()
+        intents.message_content = True
+        super().__init__(command_prefix=prefix, intents=intents)
+        self.mk8_client = MK8Client()
+        self.mk8_client.set_device(DEVICE_ID, SERIAL_NUMBER, SYSTEM_VERSION)
+        self.mk8_client.set_locale(REGION_ID, REGION_NAME, COUNTRY_ID, COUNTRY_NAME, LANGUAGE)
 
-        # Bold the score for the player with the most wins
-        p1_wins = f"**{m.p1_wins}**" if m.p1_wins > m.p2_wins else m.p1_wins
-        p2_wins = f"**{m.p2_wins}**" if m.p1_wins < m.p2_wins else m.p2_wins
-        embed.description = f"{p1_wins} – {m.ties} – {p2_wins}"
+    async def setup_hook(self):
+        await self.add_cog(Commands(self))
 
-        tracks = list(p1.records.keys())
-        p1_times = [
-            mark_time_if_faster(t1, t2) for t1, t2 in zip(p1.records.values(), p2.records.values())
-        ]
-        p2_times = [
-            mark_time_if_faster(t2, t1) for t1, t2 in zip(p1.records.values(), p2.records.values())
-        ]
-
-        categories = ["Nitro Tracks", "Retro Tracks", "DLC Tracks"]
-        for i, cat in enumerate(categories):
-            rows = "\n".join(
-                "%-4s    %10s    %10s" %
-                (tracks[i*16 + x], p1_times[i*16 + x].center(10), p2_times[i*16 + x].center(10))
-                for x in range(16)
-            )
-            embed.add_field(name=cat, value=f"```{rows}```", inline=False)
-    except RuntimeError:
-        embed.title = f"Failed to generate a matchup between {nnid1} and {nnid2}:"
-        embed.description = "Failed to fetch both timesheets; connection timed out."
-    finally:
-        await message.edit(embed=embed)
+    async def on_ready(self):
+        await self.tree.sync()
+        await self.mk8_client.login(USERNAME, PASSWORD)
+        print("We up, boys!")
 
 
-@bot.command(description="Gets stats")
-async def stats(ctx, *args):
-    embed = discord.Embed()
-    if len(args) == 0:
-        tracks = [track for track in MK8Tracks]
-        embed.title = "Stats for all tracks:"
-        embed.description = "```%s```" % format_all_stats((await get_stats(mk8_client, tracks)))
-        await ctx.reply(embed=embed)
-        return
+class Commands(commands.Cog):
+    def __init__(self, bot: BotBoi):
+        self.bot = bot
 
-    abbr = args[0]
-    try:
-        track = MK8Tracks(abbr)
-    except KeyError:
-        embed.title = f"The abbreviation {abbr} is not a known track abbreviation"
-        embed.description = "Please try again with a more commonly used abbreviation instead"
-    else:
-        embed.title = f"Stats for {track.fullname} ({track.abbr})"
-        track_stats = stats_to_labeled_dict((await get_stats(mk8_client, track))[track.abbr])
-        for stat, value in track_stats.items():
-            embed.add_field(name=stat, value=value)
-    finally:
-        await ctx.reply(embed=embed)
+    @commands.hybrid_command(aliases=["mu"], description="Gets the matchup between two players given their NNIDs")
+    async def matchup(self, ctx, nnid1: str, nnid2: str):
+        embed = discord.Embed()
+        embed.title = f"Generating matchup between {nnid1} and {nnid2}..."
+        embed.description = "Please wait..."
+        message = await ctx.reply(embed=embed)
+        try:
+            async with self.bot.mk8_client.backend_login() as bc:
+                rc = RankingClient(bc)
+                m = ts.get_matchup(*(await ts.get_timesheets(rc, [nnid1, nnid2])))
+            p1 = m.p1_timesheet
+            p2 = m.p2_timesheet
+            embed.title = (f"{p1.mii_name} ({p1.nnid})   vs.   {p2.mii_name} ({p2.nnid})"
+                           .translate(md_escape))
 
+            # Bold the score for the player with the most wins
+            p1_wins = f"**{m.p1_wins}**" if m.p1_wins > m.p2_wins else m.p1_wins
+            p2_wins = f"**{m.p2_wins}**" if m.p1_wins < m.p2_wins else m.p2_wins
+            embed.description = f"{p1_wins} – {m.ties} – {p2_wins}"
 
-@bot.command(aliases=["ts"], description="Gets the timesheet for the player with the given NNID")
-async def timesheet(ctx, nnid):
-    embed = discord.Embed()
-    embed.title = f"Fetching timesheet for {nnid}..."
-    embed.description = "Please wait..."
-    message = await ctx.reply(embed=embed)
-    try:
-        player_ts = await ts.get_timesheet(mk8_client, nnid)
-        ts_list = player_ts.to_string_array()
-        name = player_ts.mii_name.translate(md_escape)
+            tracks = list(p1.records.keys())
+            p1_times = [
+                mark_faster_time(t1, t2) for t1, t2 in zip(p1.records.values(), p2.records.values())
+            ]
+            p2_times = [
+                mark_faster_time(t2, t1) for t1, t2 in zip(p1.records.values(), p2.records.values())
+            ]
 
-        embed.title = f"Timesheet for {name} ({nnid}):" if name else f"Timesheet for {nnid}:"
-        embed.description = "Shows the track abbreviation, worldwide position*, and time for each track."
+            categories = ["Nitro Tracks", "Retro Tracks", "DLC Tracks"]
+            for i, cat in enumerate(categories):
+                rows = "\n".join(
+                    "%-4s    %10s    %10s" %
+                    (tracks[i * 16 + x], p1_times[i * 16 + x].center(10), p2_times[i * 16 + x].center(10))
+                    for x in range(16)
+                )
+                embed.add_field(name=cat, value=f"```{rows}```", inline=False)
+        except RuntimeError:
+            embed.title = f"Failed to generate a matchup between {nnid1} and {nnid2}:"
+            embed.description = "Failed to fetch both timesheets; connection timed out."
+        finally:
+            await message.edit(embed=embed)
 
-        for index, cup in enumerate(MK8Cups):
-            cup_value = "\n".join(ts_list[index*4 + x] for x in range(4))
-            embed.add_field(name=f"{cup.name.title()} Cup", value=f"```{cup_value}```")
+    @commands.command(description="Gets stats")
+    async def stats(self, ctx, *args):
+        embed = discord.Embed()
+        if len(args) == 0:
+            tracks = [track for track in MK8Tracks]
+            embed.title = "Stats for all tracks:"
+            async with self.bot.mk8_client.backend_login() as bc:
+                rc = RankingClient(bc)
+                result = await get_stats(rc, tracks)
+            embed.description = "```%s```" % format_all_stats(result)
+            await ctx.reply(embed=embed)
+            return
 
-        for i in range(len(MK8Cups)-2, 0, -2):
-            embed.insert_field_at(index=i, name='\u200B', value='\u200B', inline=False)
-
-        embed.set_footer(text="*Note that worldwide positions may be inaccurate due to hacked times and alts.")
-
-    except (KeyError, RuntimeError) as e:
-        embed.title = f"Failed to fetch the timesheet for {nnid}:"
-        if e.__class__ is KeyError:
-            embed.description = f"The NNID \"{nnid}\" does not exist."
+        abbr = args[0]
+        try:
+            track = MK8Tracks(abbr)
+        except KeyError:
+            embed.title = f"The abbreviation {abbr} is not a known track abbreviation"
+            embed.description = "Please try again with a more commonly used abbreviation instead"
         else:
-            embed.description = "Connection timed out."
-    finally:
-        await message.edit(embed=embed)
+            embed.title = f"Stats for {track.fullname} ({track.abbr})"
+            async with self.bot.mk8_client.backend_login() as bc:
+                rc = RankingClient(bc)
+                result = (await get_stats(rc, track))[track.abbr]
+            track_stats = stats_to_labeled_dict(result)
+            for stat, value in track_stats.items():
+                embed.add_field(name=stat, value=value)
+        finally:
+            await ctx.reply(embed=embed)
+
+    @commands.hybrid_command(aliases=["ts"], description="Gets the timesheet for the player with the given NNID")
+    async def timesheet(self, ctx, nnid):
+        embed = discord.Embed()
+        embed.title = f"Fetching timesheet for {nnid}..."
+        embed.description = "Please wait..."
+        message = await ctx.reply(embed=embed)
+        try:
+            async with self.bot.mk8_client.backend_login() as bc:
+                rc = RankingClient(bc)
+                player_ts = await ts.get_timesheet(rc, nnid)
+            ts_list = player_ts.to_string_array()
+            name = player_ts.mii_name.translate(md_escape)
+
+            embed.title = f"Timesheet for {name} ({nnid}):" if name else f"Timesheet for {nnid}:"
+            embed.description = "Shows the track abbreviation, worldwide position*, and time for each track."
+
+            for index, cup in enumerate(MK8Cups):
+                cup_value = "\n".join(ts_list[index * 4 + x] for x in range(4))
+                embed.add_field(name=f"{cup.name.title()} Cup", value=f"```{cup_value}```")
+
+            for i in range(len(MK8Cups) - 2, 0, -2):
+                embed.insert_field_at(index=i, name='\u200B', value='\u200B', inline=False)
+
+            embed.set_footer(text="*Note that worldwide positions may be inaccurate due to hacked times and alts.")
+
+        except (KeyError, RuntimeError) as e:
+            embed.title = f"Failed to fetch the timesheet for {nnid}:"
+            if e.__class__ is KeyError:
+                embed.description = f"The NNID \"{nnid}\" does not exist."
+            else:
+                embed.description = "Connection timed out."
+        finally:
+            await message.edit(embed=embed)
 
 
-bot.run(os.getenv("TOKEN"))
+async def main():
+    async with BotBoi() as bot:
+        await bot.start(os.getenv("TOKEN"))
+
+asyncio.run(main())
